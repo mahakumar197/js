545457f841e574ff5cab8bdca55b665af4e35335
mahalakshmi <mahalakshmi.j@swordgroup.in>
2026-02-11 12:18:19 +0530

    js

---
+++ dataType.js
@@ -0,0 +1,85 @@
+// camel case hiTeamWelcome
+
+// DataType
+
+// data type - define a data - what kind of a data it is, 
+// variable - is a container, it is holds a value  
+
+
+// let name = "maha"
+
+// 2 types of dataType
+
+// 1.primitive data type - it stores single value(data) in a single data type
+// 2.Non primive data type - it stores multiple value(data) in a single variable 
+
+// rahgul - text - string ("",'')
+// string
+// let name = "maha";
+// let name1 ='amid';
+
+// number
+
+// let age = 20;
+
+// boolean 
+
+// let islogin = yes;  //yes, no
+
+// undifined - we declare a variable not assign the data 
+
+let mobile;
+mobile ="green"; // when ever we want we can assign the value 
+
+console.log(mobile);
+
+// null - null value - 0
+
+let greetings = null;
+console.log(greetings);
+
+// gigInt - large number - 23456789012345678
+// symbols
+
+// non- primitive data type 
+// multiple values stores in a single variable - handle complex data 
+
+// object - stores multiple value together - we called it as key value pair
+
+// variable name must be unique
+
+let person ={
+    names : "raghul",   // names - key rahgul - value
+    age : 26, 
+    city : "padur"
+}
+console.log(person);
+console.log(person.age,person.city);
+
+
+// Array - stores multple value in order
+
+let arr = ["a","b","c","d","e"];
+         // 0   1   2   3   4     - index number
+
+         console.log(arr[3]); // inside square bracket we can access
+
+// function - stored instruction  / reuse 
+
+// function greet1(){
+//     console.log("hello");
+// }
+
+// greet1();  //call a function
+// greet1();
+// greet1();
+// greet1();
+// greet1();
+// greet1();
+// greet1();
+
+// typeof - tell what a data type is 
+
+
+let member = "data";
+console.log(typeof member);
