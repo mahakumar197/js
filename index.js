fff0e444034df754cfec82bbb50fd6edce6746b1
mahalakshmi <mahalakshmi.j@swordgroup.in>
2026-02-13 17:22:31 +0530

    day 4

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ console.log("hi");
 
 // data type is - type of data for eg string
 
+let a = 10; // - a variable 
+             //- let keyword - use to create a variable 
+             //- 10 Data(value)
+
+             const b =20;
+             console.log(a,b);
+             a=30;
+             console.log(a, b);
+            //  b=40;
+             console.log(a, b);
+             var c =10;
+             c =30;
+
+
+let lastNameWillBeData;
+let name665;
+let at$jgerg
---
+++ operators.js
@@ -0,0 +1,51 @@
+// operators - is used do an operation
+
+let a = 10;
+let b = 20;
+//arithmatic
+// console.log(a+b); // here + is operator(add) a,b operend
+// console.log(b-a);  //10-20
+// console.log(a*b);
+// console.log(b/a);
+// console.log(b%a);
+
+// //increment ++
+// console.log(a++,"increment"); //a+1 10 post increment
+// console.log(a); // 11
+// console.log(++a); /// pre increment 1+a + 1+11=12
+// console.log(b,"b")
+// console.log(b++);
+// console.log(b);
+// console.log(++b);
+// decrement
+// console.log(a);
+// console.log(a--); //post decrement
+// console.log(a);
+// console.log(--a);
+
+// assignment operator  -- (=,+=,-=,*=,/=,%=)
+
+// console.log(a+=b); // a=a+b
+// console.log(a);
+// console.log(a-=b); //a=a-b
+// console.log(a*=b); //a=a*b
+// console.log(a/=b); //a=a/b
+// console.log(a);
+// console.log(a%=b); //a=a%b
+
+// //**= exponent & assign
+// let c= 2
+// c **=3 //2*2*2
+// console.log(c);
+
+//comaprison operator 
+
+// == to check both are same
+// === 1. to check both values are same 2. both data types are same 
+// < --Less then
+// > --greater then
+//<= - less then or equal 
+//>= -graeter than or equal
+
+// 
+
